c35cf842330447e5813825e293d5f80af38354d6
Nick Askam <nickaskam@gmail.com>
2021-11-29 16:54:34 -0800

    first commit

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+// reads
+
 app.get('/customers', function(req, res) {
   let query = 'SELECT * FROM Customers;'
 
@@ -81,6 +83,132 @@ app.get('/coffee-orders', function(req, res) {
 
   db.pool.query(query, function (error, rows, fields) {
     res.render('coffee-orders', { data: rows });
+  })
+})
+
+// inserts
+app.post('/addBeansCoffee', function (req, res) {
+  let data = req.body
+
+  let query1 = `INSERT INTO BeansWithCoffees (beanID, coffeeID) VALUES ('${data.beanID}', '${data.coffeeID}')`
+
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400)
+    } else {
+      res.redirect('/beans-coffee')
+    }
+  })
+})
+
+app.post('/addCoffeeOrders', function (req, res) {
+  let data = req.body
+
+  let query1 = `INSERT INTO CoffeeOrders (orderID, coffeeID) VALUES ('${data.orderID}', '${data.coffeeID}')`
+
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400)
+    } else {
+      res.redirect('/coffee-orders')
+    }
+  })
+})
+
+app.post('/addTeaOrders', function (req, res) {
+  let data = req.body
+
+  let query1 = `INSERT INTO TeaOrders (orderID, teaID) VALUES ('${data.orderID}', '${data.teaID}')`
+
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400)
+    } else {
+      res.redirect('/tea-orders')
+    }
+  })
+})
+
+app.post('/newCoffee', function (req, res) {
+  let data = req.body
+
+  let query1 = `INSERT INTO Coffees (type, volumeOfCoffeeInGrams, volumeOfWaterInLiters, additive, brewTime, price, specialRequest) 
+                VALUES ('${data.type}', '${data.volumeOfCoffeeInGrams}', '${data.volumeOfWaterInLiters}', '${data.additive}', '${data.brewTime}', '${data.price}', '${data.specialRequest}')`
+
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400)
+    } else {
+      res.redirect('/coffees')
+    }
+  })
+})
+
+app.post('/addBean', function (req, res) {
+  let data = req.body
+
+  let query1 = `INSERT INTO Beans (countryOfOrigin, roastType, roastDate)
+                VALUES ('${data.countryOfOrigin}', '${data.roastType}', '${data.roastDate}')`
+
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400)
+    } else {
+      res.redirect('/beans')
+    }
+  })
+})
+
+app.post('/addCustomer', function (req, res) {
+  let data = req.body
+
+  let query1 = `INSERT INTO Customers (firstName, lastName, rewardSpend, email)
+                VALUES ('${data.firstName}', '${data.lastName}', '${data.rewardSpend}', '${data.email}')`
+
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400)
+    } else {
+      res.redirect('/customers')
+    }
+  })
+})
+
+app.post('/addOrder', function (req, res) {
+  let data = req.body
+
+  let query1 = `INSERT INTO Orders (orderDate, orderTotal, customerID)
+                VALUES ('${data.orderDate}', '${data.orderTotal}', '${data.customerID}')`
+
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400)
+    } else {
+      res.redirect('/orders')
+    }
+  })
+})
+
+app.post('/addTea', function (req, res) {
+  let data = req.body
+
+  let query1 = `INSERT INTO Teas (origin, color, matcha, flavor, season, price) 
+                VALUES ('${data.origin}', '${data.color}', '${data.matcha}', '${data.flavor}', '${data.season}', '${data.price}')`
+
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400)
+    } else {
+      res.redirect('/teas')
+    }
   })
 })
 
